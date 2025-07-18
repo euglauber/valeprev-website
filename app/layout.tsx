38ce2c43f5ae80277538e1a24c69af39859c1627
Glauber <glaubervinicius2018@gmail.com>
2025-07-18 00:27:25 -0300

    Atualizações no titulo da page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Escritório Vale-Prev',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Vale-Prev Escritório',
+  description: 'Created with VSCode',
+  generator: 'eu.glauber',
 }
 
 export default function RootLayout({
